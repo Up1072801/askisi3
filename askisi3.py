56e9e59a18d9ebc4e7a7eb7d8c029bcd3b3583f0
Up1072801 <125863183+Up1072801@users.noreply.github.com>
2023-05-29 23:11:11 +0300

    Add files via upload

---
+++ askisi3.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+import requests
+broker = "test.mosquitto.org"
+port = 1883
+topic = "http/post"
+url = "https://httpbin.org/post"
+headers = {"Content-Type": "application/json"}
+payload = {"key": "Hello"}
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker")
+    client.publish(topic, str(payload))
+def on_publish(client, userdata, mid):
+    print("Message published to MQTT topic")
+    send_http_request()
+def send_http_request():
+    response = requests.request("POST", url, headers=headers, data=str(payload))
+    print("HTTP response: " + str(response.status_code))
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.connect(broker, port, 60)
+client.loop_forever()
